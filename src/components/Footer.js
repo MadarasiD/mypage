67b192a6e008dbd0f21717a242fc68ea429e3393
MadarasiD <madarka0913@gmail.com>
2022-06-03 23:20:21 +0200

    Footer ready

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,14 +7,18 @@ const Footer = () => {
         <div className='md:w-full w-1/2'>
                   <div className='bg-neutral-800 p-10 font-lob text-neutral-400 text-center '>
                   
-                  <p> Developed and created - Madarasi Dániel</p>
-                  <div className='flex justify-between my-5 w-full text-white text-3xl'>
-                    <FaFacebook />
-                    <FaGithub />
-                    <FaInstagram />
-                    <FaLinkedin />
+                  
+                  <div className='flex justify-between my-5 w-full text-white text-3xl border-b-4'>
+                    <div className='flex justify-between my-5 w-full text-white text-3xl '>
+                    <FaFacebook className='hover:text-red-700 hover:text-4xl hover:transition-all hover:transition-ease-in-out'/>
+                    <FaGithub className='hover:text-red-700 hover:text-4xl hover:transition-all hover:transition-ease-in-out'/>
+                    <FaInstagram className='hover:text-red-700 hover:text-4xl hover:transition-all hover:transition-ease-in-out'/>
+                    <FaLinkedin className='hover:text-red-700 hover:text-4xl hover:transition-all hover:transition-ease-in-out'/>
+                    </div>
                   </div>
 
+
+                  <p> Developed and created - Madarasi Dániel</p>
 
                 </div>
           </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -405,15 +405,17 @@ const Home = () => {
 
         {/* Contact section start*/}
 
-        <div className="bg-gradient-to-b from-neutral-400	 via-neutral-600 to-neutral-800">
+        <div className="bg-gradient-to-b from-neutral-400	 via-neutral-600 to-neutral-800 flex justify-center">
 
-        <div className='md:w-full w-1/2 p-10 shadow-lg shadow-indigo-200	'>
-                    <h1 className='text-2xl text-white font-semibold '>Contact me</h1>
-                    <input type="text"  placeholder='name' className='w-full border-2  border-gray-900 rounded-3xl py-1 shadow-md shadow-gray-100 mt-5'/>
-                    <input type="email" placeholder='email' className='w-full border-2  border-gray-900 rounded-3xl py-1 shadow-md shadow-gray-100 mt-5'/>
-                    <textarea type="text" placeholder='query' className='w-full border-2  border-gray-900 rounded-3xl py-1 shadow-md shadow-gray-100 mt-5'/>
-
-                    <button className='border-2  bg-red-200 text-white rounded-3xl hover:bg-red-600 px-5 py-1'>SUBMIT</button>
+               <div className='md:w-full w-1/2 p-10  shadow-lg shadow-indigo-200	'>
+                 
+                    <h1 className='text-2xl text-white text-center font-semibold '>Contact me</h1>
+                    <input type="text"  placeholder=' name' className='w-full border-2  border-gray-900 rounded-3xl py-1 text-center shadow-md shadow-gray-100 mt-5'/>
+                    <input type="email" placeholder=' email' className='w-full border-2  border-gray-900 rounded-3xl py-2 text-center shadow-md shadow-gray-100 mt-5'/>
+                    <textarea type="textarea" placeholder=' query' className='w-full border-2  border-gray-900 rounded-3xl text-center shadow-md shadow-gray-100 mt-5'/>
+                    <div className='flex justify-center '>
+                    <button className='border-2 mt-3 bg-red-800 text-white rounded-3xl hover:bg-red-600 px-5 py-1'>SUBMIT</button>
+                    </div>
                 </div>
                 
            
