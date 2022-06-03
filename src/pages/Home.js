9c2fc6f9a3d3d64faf5e4d9e0cb5f81dd476ac97
MadarasiD <madarka0913@gmail.com>
2022-06-03 22:01:54 +0200

    contact section start

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import '../styles/Home.css'
 import {FaHtml5, FaCss3, FaReact, FaBootstrap, FaSass, FaGithub, FaNodeJs} from 'react-icons/fa'
 import {SiTailwindcss} from 'react-icons/si'
 import {TbBrandJavascript} from 'react-icons/tb'
-import "react-simple-typewriter/dist/index"
+
 
 
 
@@ -146,7 +146,6 @@ const Home = () => {
         {/* Skills section end*/}
 
         {/* About section start*/}
-
         <div className='bg-gradient-to-b from-neutral-400	 via-neutral-600 to-neutral-800 ' >
           <div className='text-center'>
 
@@ -402,10 +401,39 @@ const Home = () => {
 
             </div>
         </div>
-
         {/* Project section end*/}
 
         {/* Contact section start*/}
+
+        <div className="bg-gradient-to-b from-neutral-400	 via-neutral-600 to-neutral-800">
+
+            <div className="grid md:grid-cols-1 grid-cols-2 items-center ">
+
+              <div className='font-bold text-center flex justify-center md:px-3 '>
+                
+                <div className='md:w-full w-1/2 p-10 shadow-lg shadow-indigo-200	'>
+                    <h1 className='text-2xl text-white font-semibold '>Contact me</h1>
+                    <input type="text"  placeholder='name' className='w-full border-2  border-gray-900 rounded-3xl py-1 shadow-md shadow-gray-100 mt-5'/>
+                    <input type="email" placeholder='email' className='w-full border-2  border-gray-900 rounded-3xl py-1 shadow-md shadow-gray-100 mt-5'/>
+                    <textarea type="text" placeholder='query' className='w-full border-2  border-gray-900 rounded-3xl py-1 shadow-md shadow-gray-100 mt-5'/>
+
+                    <button className='border-2  bg-red-200 text-white rounded-3xl hover:bg-red-600 px-5 py-1'>SUBMIT</button>
+                </div>
+                
+              </div>
+
+              
+
+              <div className='grid md:grid-cols-1 grid-cols-2 h-screen '>
+                <lottie-player src="https://assets4.lottiefiles.com/private_files/lf30_WdTEui.json" background="transparent" speed="1" loop autoplay></lottie-player>
+              </div>
+
+            </div>
+
+          </div>
+
+        {/* Contact section end*/}
+
       </div>
     </Layout>
   )
