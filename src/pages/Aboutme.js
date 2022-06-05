45b345f52ffc24b3889754277f09d83ce4ffe9e2
MadarasiD <madarka0913@gmail.com>
2022-06-05 22:18:32 +0200

    continued tomorrow

--- src/pages/Aboutme.js
+++ src/pages/Aboutme.js
@@ -3,66 +3,37 @@ import Layout from '../components/Layout'
 
 const Aboutme = () => {
   return (
-    <Layout>
-        <div>
-        <div className="h-screen bg-gradient-to-b from-neutral-800	 via-neutral-600 to-neutral-800 pt-20">
+    <Layout >
+        
+        
 
-          <div className="grid md:grid-cols-1 grid-cols-2 items-center transfrom transition skew-y-12 md:skew-y-0 ">
+          <div className=" bg-gradient-to-b from-neutral-800	 via-neutral-600 to-neutral-800 ">
 
-            <div className='font-bold 	 text-center justify-center md:px-3 '>
-              <h1 className='text-4xl  text-emerald-700	md:text-2xl md:pt-32 animate-pulse tarnsform transition-all animate-bounce '>
-                Hi, my name is<br /> <b className='text-emerald-400	text-5xl '>
-                
-                  Madarasi Dániel</b></h1>
-                  
+            <div className='text-center  h-full relative'>
+            <lottie-player src="https://assets4.lottiefiles.com/packages/lf20_MvFIQ6.json"  background="transparent"  speed="1"    loop  autoplay></lottie-player>
+            </div>
+
+            
+
+            <div className='absolute inset-0 flex flex-col items-center justify-center pt-80'>
+              <div >
+              <h1 className='text-4xl pt-60'>Hi, it's Dani. Welcome to my website</h1>
               
-              <h1 className='text-3xl  text-teal-500	ms:text-xl tarnsform transition-all animate-bounce'>
-                  Frontend <b className='text-red-600'>Developer</b>
-              </h1>
+              <pre className='items-center pt-32'>
+               My development studies at Frontend began in early 2020. I’ve always<br />been interested in websites, now I can finally immerse myself in it.<br />I haven’t worked in the profession yet, but I’ve done and learned a<br />lot of projects.
+                I also like to work in a team, <br />but if I have to work alone, that’s not a problem. <br />I study in my free time, I am constantly <br />improving my existing knowledge.<br />
+                I am open to learning new things and I love challenges.
+                </pre>
+              </div>
             </div>
-
-            <div id="parallax">
-
-              <div className="header-shape shape-one  hover:text-lg">
-                      <img  src={require("../assets/HTML5.png")} alt="" />
-              </div>
-
-              <div className="header-shape shape-two">
-                      <img src={require("../assets/CSS.png")} alt="" />
-              </div>
-
-              <div className="header-shape shape-three">
-                      <img src={require("../assets/js.jpg")} alt="" />
-              </div>
-
-              <div className="header-shape shape-fore">
-                      <img src={require("../assets/Bootstrap.png")} alt="" />
-              </div>
-
-              <div className="header-shape shape-five">
-                      <img src={require("../assets/REACT.png")} alt="" />
-              </div>
-
-              <div className="header-shape shape-six">
-                      <img src={require("../assets/Angular.png")} alt="" />
-              </div>
-
-              <div className="header-shape shape-seven">
-                      <img src={require("../assets/Photoshop2.png")} alt="" />
-              </div>
-
-                
-            </div>
-
-
-            <div className='grid md:grid-cols-1  grid-cols-2 h-screen '>
-              <lottie-player src="https://assets4.lottiefiles.com/private_files/lf30_WdTEui.json" background="transparent" speed="1" loop autoplay></lottie-player>
-            </div>
+            
+            
 
           </div>
+          
 
-          </div>
-        </div>
+          
+        
     </Layout>
   )
 }
